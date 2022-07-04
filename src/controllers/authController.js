6f62f61fbb858304504ab7ea80c449e3b6746802
elias-soykat <eliasmd624@gmail.com>
2022-07-04 17:05:52 +0600

    project init

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -36,6 +36,11 @@ exports.registerUser = asyncHandler(async (req, res) => {
 exports.loginUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
 
+  if (!email || !password) {
+    res.status(400);
+    throw new Error("Please add all fields");
+  }
+
   const user = await User.findOne({ email });
 
   const isPasswordMatched = await bcrypt.compare(password, user.password);
